34db99d303a9142852ebf21762a6fd2d65e40bd2
KuBerlinON <bkuberlinov@gmail.com>
2021-05-17 18:25:41 +0200

    connecting react with express

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.use((req, res) => {
-    res.send('Hello Buskers')
+app.set('PORT', PORT);
+
+//testing the requests and connection
+const users = []
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.post('/login', (req, res) => {
+    users.push(req.body)
+    console.log(req.body)
+    console.log('user data received')
+    console.log(users)
+})
+
+app.post('/user', (req, res) => {
+    res.send(users)
 })
 
 app.listen(PORT, () => {
     console.log('Server 8080 is up!')
 })
+
