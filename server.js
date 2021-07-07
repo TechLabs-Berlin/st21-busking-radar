8ae09610f83861c71928e6e0d81bc274fe6dfb6f
KuBerlinON <bkuberlinov@gmail.com>
2021-07-07 16:38:01 +0200

    preparing to deploy

--- server.js
+++ server.js
@@ -10,8 +10,23 @@ const PORT = process.env.PORT || 8080;
 app.set('PORT', PORT);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:8080',]
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log("** Origin of request " + origin)
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log("Origin acceptable")
+            callback(null, true)
+        } else {
+            console.log("Origin rejected")
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
 
 //importing routes
 const eventRoutes = require('./routes/events.js');
@@ -30,7 +45,14 @@ app.use('/auth', authRoutes);
 app.use('/geocoding', geocodingRoutes);
 
 
-
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 //connecting to mongoose => connection url must be secured later!!! after mongodb+srv should be the username and password written
 //in order to be able to connect ask me for the password and username, but please always delete them before pushing to github
@@ -46,5 +68,8 @@ mongoose.connect(CONNECTION_URL, {
 }).catch((e) => {
     console.log('This did not work', e.message)
 })
+
+
+
 mongoose.set('useFindAndModify', false)
 
